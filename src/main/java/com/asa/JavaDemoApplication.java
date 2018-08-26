378819564131db55807884a8f15eb91cfab37959
479804091@qq.com <479804091@qq.com>
2018-08-26 19:02:06 +0800

    提交Java项目

---
+++ src/main/java/com/asa/JavaDemoApplication.java
@@ -0,0 +1,12 @@
+package com.asa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDemoApplication.class, args);
+	}
+}
